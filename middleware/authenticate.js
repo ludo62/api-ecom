41598b75da0a6de2d612af4f64e853cc6725ad27
ludo62 <fournier.ludovic@orange.fr>
2024-01-05 13:30:47 +0100

    middleware

---
+++ middleware/authenticate.js
@@ -0,0 +1,32 @@
+// Middleware d'authentification
+const jwt = require('jsonwebtoken');
+const authModel = require('../models/auth.model');
+
+module.exports.authenticate = async (req, res, next) => {
+	try {
+		const authHeader = req.header('Authorization');
+
+		if (!authHeader || !authHeader.startsWith('Bearer ')) {
+			return res
+				.status(401)
+				.json({ message: 'Vous devez être connecté pour accéder à cette ressource' });
+		}
+
+		// Extraction du token sans le préfixe "Bearer"
+		const token = authHeader.split(' ')[1];
+
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		const user = await authModel.findById(decoded.user.id);
+
+		if (!user) {
+			return res.status(401).json({ message: 'Utilisateur non trouvé' });
+		}
+
+		req.user = user;
+		next();
+	} catch (error) {
+		console.error("Erreur lors de l'authentification :", error.message);
+		res.status(500).json({ message: "Erreur lors de l'authentification", error });
+	}
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,29 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
+const authMiddleware = require('../middleware/authenticate');
 
 // Route pour l'inscription
 router.post('/register', authController.register);
 
 // Route pour la connexion
 router.post('/login', authController.login);
+
+// Exemple de route protégée nécessitant une authentification
+router.get('/protected-route', authMiddleware.authenticate, (req, res) => {
+	// Vérifier si l'utilisateur est un administrateur
+	if (req.user.role === 'admin') {
+		// Tâches spécifiques pour les administrateurs
+		// Ici, je définis req.isAdmin à true pour les administrateurs
+		req.isAdmin = true;
+
+		// Envoyer une réponse pour les administrateurs
+		return res.status(200).json({ message: 'Bienvenue, administrateur!' });
+	} else {
+		// Envoyer une réponse pour les utilisateurs non-administrateurs
+		return res.status(403).json({
+			message: 'Action non autorisée. Seuls les administrateurs ont accès à cette ressource.',
+		});
+	}
+});
 
 module.exports = router;
